de68b9edf6ea616fa572d9425888191d4c718106
Felipe Jung <felipemattoseu@gmail.com>
2020-08-08 14:31:57 -0400

    chore(initial commit): :tada: project initial commit
    strapi app with initial structure, custom rich text editor and inital database relationships

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '44dc3ca672ba3160b71a20517e10faed'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '36438883-0336-4ad5-8237-5ec20a46ae26'
+};
